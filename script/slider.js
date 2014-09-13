b64bc2f0d5fa2f7e766a0175ebdfafc9956d7a98
yawB <boakye.yawyaw@gmail.com>
2014-09-13 14:10:10 -0400

    Initial Commit
    WEDB 221/complex/blog/style/site/assignment1/week2

---
+++ script/slider.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	$('.banner').unslider({
+	speed: 500,               //  The speed to animate each slide (in milliseconds)
+	delay: 4500,              //  The delay between slide animations (in milliseconds)
+	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+	dots: true,               //  Display dot navigation
+	fluid: false              //  Support responsive design. May break non-responsive designs
+	});
+});
